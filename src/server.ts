185908149ca6ca2a9a4c55073717ee528d9ffe31
Fermín Morello <ferminmorello@gmail.com>
2023-07-02 19:48:25 -0300

    Included PORT from config

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,31 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Response, Request } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { PORT } from './config/config';
 
 class ServerBootstrap {
   public app: express.Application = express();
+  private port: number = Number(PORT);
 
   constructor() {
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(morgan('dev'));
     this.app.use(cors());
+
     this.listen();
   }
 
-  public listen(): void {
-    this.app.listen(3000, () => {
-      console.log('Server listening on port => ' + 3000);
-    });
+  public routers(): Array<express.Router> {
+    return [];
   }
 
+  public listen(): void {
+    this.app.listen(this.port, () => {
+      console.log('Server listening on port => ' + this.port);
+    });
+  }
 }
 
 new ServerBootstrap();
