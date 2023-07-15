e0033bc792a819a297f93e5ec6a18bc8f52fbafb
Fermín Morello <ferminmorello@gmail.com>
2023-07-15 20:38:17 -0300

    Se agrega el atributo de la relación de user category

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,4 +1,5 @@
 import { BaseEntity } from '../../config/base.entity';
+import { UserCategoryEntity } from '../../custom/entities/user-category.entity';
 import { ExpenseEntity } from '../../expense/entities/expense.entity';
 import { Entity, Column, OneToMany, Index } from 'typeorm';
 
@@ -10,4 +11,10 @@ export class CategoryEntity extends BaseEntity {
 
   @OneToMany(() => ExpenseEntity, expense => expense.category)
   expenses!: ExpenseEntity[];
+
+  @OneToMany(
+    () => UserCategoryEntity,
+    (UserCategory) => UserCategory.category
+  )
+  UserCategory!: UserCategoryEntity;
 }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -2,6 +2,7 @@ import { Entity, Column, OneToMany, JoinColumn } from 'typeorm';
 import { Exclude } from 'class-transformer';
 import { BaseEntity } from '../../config/base.entity';
 import { ExpenseEntity } from '../../expense/entities/expense.entity';
+import { UserCategoryEntity } from '../../custom/entities/user-category.entity';
 
 @Entity({ name: 'user' })
 export class UserEntity extends BaseEntity {
@@ -17,4 +18,10 @@ export class UserEntity extends BaseEntity {
 
   @OneToMany(() => ExpenseEntity, (expense) => expense.user)
   expenses!: ExpenseEntity[];
+
+  @OneToMany(
+    () => UserCategoryEntity,
+    (UserCategory) => UserCategory.user
+  )
+  UserCategory!: UserCategoryEntity;
 }
