dcc32bb09734d1d270c470af9dad2dbd02724df5
Fermín Morello <ferminmorello@gmail.com>
2023-07-15 20:43:19 -0300

    Export of UserCategory Router

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { UserRouter } from './user/user.router';
 export { CategoryRouter } from './category/category.router';
 export { ExpenseRouter } from './expense/expense.router';
 export { AuthRouter } from './auth/auth.router';
+export { UserCategoryRouter } from './custom/routers/user-category.router';
